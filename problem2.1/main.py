a18b4e191c62086cc37c6665f713f0a73a599b45
alisofyansaputra <Buffware4@gmail.com>
2024-02-20 14:19:17 +0700

    bilangan.man.py

--- problem2.1/main.py
+++ problem2.1/main.py
@@ -1,1 +1,9 @@
 bilangan = int(input())
+faktor = []
+for i in range(1, bilangan +1):
+    if bilangan % i == 0:
+        faktor.append(i)
+
+print(f"angka dari bilangan {bilangan} adalah:")
+for f in faktor:
+    print(f)
