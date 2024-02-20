fd73147cbf982408780dc5540111e7915bc5eedf
alisofyansaputra <Buffware4@gmail.com>
2024-02-20 23:46:21 +0700

    bilangan.main.py

--- problem2.2/main.py
+++ problem2.2/main.py
@@ -1,1 +1,13 @@
 bilangan = int(input())
+def cetak_faktor(bilangan):
+    for i in range(bilangan, 0, -1):
+        if bilangan % i == 0:
+            print(i)
+
+input_bilangan_1 = 6
+print(f"bilangan {input_bilangan_1} adalah:")
+cetak_faktor(input_bilangan_1)
+
+input_bilangan_2 = 20
+print(f"\nbilangan {input_bilangan_2} adalah:")
+cetak_faktor(input_bilangan_2)
